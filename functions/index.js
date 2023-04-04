4b0f8c3c55651f3894d46a1284e695312ad9c75c
Leratech <mattia.lerario@gmail.com>
2023-04-05 01:33:42 +0200

    API google places

--- functions/index.js
+++ functions/index.js
@@ -1,103 +1,43 @@
 const functions = require("firebase-functions");
-const puppeteer = require("puppeteer");
 const express = require("express");
 const cors = require("cors");
-const debug = require("debug")("scrapeGoogleMapsActivities");
+const axios = require("axios");
+
 const app = express();
-const addProxyTo = require("puppeteer-page-proxy");
 app.use(cors({ origin: true }));
 
-const categories = [
-  { id: 1, name: "Boxing", selected: true },
-  // ... more categories here
-  { id: 24, name: "Tennis", selected: true },
-];
-
-async function scrapeGoogleMapsActivities() {
-  console.log("scrapeGoogleMapsActivities() called");
-  try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-web-security",
-      ],
-    });
-    const page = await browser.newPage();
-
-    // Add a proxy to fetch the page content
-    await addProxyTo(page, "http://localhost:8080/");
-
-    const results = [];
-
-    for (const category of categories) {
-      if (category.selected) {
-        await page.goto(
-          `https://www.google.com/maps/search/${encodeURIComponent(
-            category.name
-          )}`
-        );
-
-        const activityData = await page.evaluate(() => {
-          const activityElements = Array.from(
-            document.querySelectorAll(".bfdHYd.Ppzolf.OFBs3e")
-          );
-          console.log("activityElements:", activityElements);
-          return activityElements.map((element) => {
-            const name = element.querySelector(".qBF1Pd").innerText;
-            const rating = element.querySelector(".ZkP5Je").innerText;
-            const address = element.querySelectorAll(".W4Efsd")[0].innerText;
-            const status = element.querySelectorAll(".W4Efsd")[1].innerText;
-
-            const img = element.querySelector("img");
-            const imageSrc = img ? img.getAttribute("src") : null;
-
-            return {
-              name,
-              rating,
-              address,
-              status,
-              imageSrc,
-            };
-          });
-        });
-
-        results.push({
-          category: category.name,
-          activities: activityData,
-        });
-        console.log("results:", results);
-      } else {
-        console.log(`Skipping category ${category.name}`);
-      }
-    }
-
-    await browser.close();
-    return results;
-  } catch (error) {
-    console.error("Error occurred:", error);
-  }
-}
+const apiKey = "AIzaSyATUCcXgflPYPqhI4D-QLdP4NpS6Xez_HI";
 
 app.get("/", async (req, res) => {
   try {
-    const seen = [];
-    const results = await scrapeGoogleMapsActivities();
-    const response = JSON.stringify(results, (key, value) => {
-      if (typeof value === "object" && value !== null) {
-        if (seen.indexOf(value) !== -1) {
-          return;
-        }
-        seen.push(value);
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/textsearch/json",
+      {
+        params: {
+          query: "boxing",
+          type: "gym",
+          key: apiKey,
+        },
       }
-      return value;
-    });
-    res.status(200).send(response);
+    );
+
+    const results = response.data.results.map((result) => ({
+      name: result.name,
+      rating: result.rating || "",
+      address: result.formatted_address,
+      status: "",
+      imageSrc: result.photos
+        ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos[0].photo_reference}&key=${apiKey}`
+        : null,
+    }));
+
+    res
+      .status(200)
+      .send(JSON.stringify({ category: "Boxing", activities: results }));
   } catch (error) {
     res.status(500).send({
       error,
-      message: "An error occurred while scraping activities.",
+      message: "An error occurred while fetching activities.",
       req,
     });
   }
