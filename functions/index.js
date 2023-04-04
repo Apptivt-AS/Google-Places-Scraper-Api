c21f25e8bc4157660f8ed16d01d91f9bbc3345de
Leratech <mattia.lerario@gmail.com>
2023-04-04 23:11:11 +0200

    Editex Proxy

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const debug = require("debug")("scrapeGoogleMapsActivities");
 const app = express();
-
+const addProxyTo = require("puppeteer-page-proxy");
 app.use(cors({ origin: true }));
 
 const categories = [
@@ -15,57 +15,69 @@ const categories = [
 
 async function scrapeGoogleMapsActivities() {
   console.log("scrapeGoogleMapsActivities() called");
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  const results = [];
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-web-security",
+      ],
+    });
+    const page = await browser.newPage();
 
-  for (const category of categories) {
-    if (category.selected) {
-      await page.goto(
-        `https://www.google.com/maps/search/${encodeURIComponent(
-          category.name
-        )}`
-      );
+    // Add a proxy to fetch the page content
+    await addProxyTo(page, "http://localhost:8080/");
 
-      const activityData = await page.evaluate(() => {
-        const activityElements = Array.from(
-          document.querySelectorAll(".bfdHYd.Ppzolf.OFBs3e")
+    const results = [];
+
+    for (const category of categories) {
+      if (category.selected) {
+        await page.goto(
+          `https://www.google.com/maps/search/${encodeURIComponent(
+            category.name
+          )}`
         );
-        console.log("activityElements:", activityElements);
-        return activityElements.map((element) => {
-          const name = element.querySelector(".qBF1Pd").innerText;
-          const rating = element.querySelector(".ZkP5Je").innerText;
-          const address = element.querySelectorAll(".W4Efsd")[0].innerText;
-          const status = element.querySelectorAll(".W4Efsd")[1].innerText;
 
-          const img = element.querySelector("img");
-          const imageSrc = img ? img.getAttribute("src") : null;
+        const activityData = await page.evaluate(() => {
+          const activityElements = Array.from(
+            document.querySelectorAll(".bfdHYd.Ppzolf.OFBs3e")
+          );
+          console.log("activityElements:", activityElements);
+          return activityElements.map((element) => {
+            const name = element.querySelector(".qBF1Pd").innerText;
+            const rating = element.querySelector(".ZkP5Je").innerText;
+            const address = element.querySelectorAll(".W4Efsd")[0].innerText;
+            const status = element.querySelectorAll(".W4Efsd")[1].innerText;
 
-          return {
-            name,
-            rating,
-            address,
-            status,
-            imageSrc,
-          };
+            const img = element.querySelector("img");
+            const imageSrc = img ? img.getAttribute("src") : null;
+
+            return {
+              name,
+              rating,
+              address,
+              status,
+              imageSrc,
+            };
+          });
         });
-      });
 
-      results.push({
-        category: category.name,
-        activities: activityData,
-      });
-      console.log("results:", results);
-    } else {
-      console.log(`Skipping category ${category.name}`);
+        results.push({
+          category: category.name,
+          activities: activityData,
+        });
+        console.log("results:", results);
+      } else {
+        console.log(`Skipping category ${category.name}`);
+      }
     }
-  }
 
-  await browser.close();
-  return results;
+    await browser.close();
+    return results;
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
 }
 
 app.get("/", async (req, res) => {
